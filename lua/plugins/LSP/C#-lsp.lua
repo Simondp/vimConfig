cd5190f6f9e2fd486c778818fd8f7993d31ce32b
Simondp <simondavidpedersen@gmail.com>
2023-01-04 19:55:16 +0100

    Removed unused folders

--- lua/plugins/LSP/C#-lsp.lua
+++
@@ -1,9 +0,0 @@
-local lspconfig = require 'lspconfig'
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-lspconfig.omnisharp.setup{
-     cmd = {"dotnet","/home/sdp/bin/omnisharp-linux-x64-net6.0/OmniSharp.dll"},
-	 filetypes={'cs','cshtml'},
-     on_attach=require('plugins/LSP/on_attach').on_attach
-}
-
--- lua/plugins/telescope/telescope.lua
+++
@@ -1,7 +0,0 @@
-require('telescope').setup{
-	defaults ={
-		prompt_prefix = "> ",
-		selection_caret = "> "
-	}
-}
---require('telescope').load_extension('fzf')
